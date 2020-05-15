07932aa8b985cc1ba23c4661e2461e5cc69a128a
Harshad <naikharshad01@gmail.com>
2020-05-15 23:26:20 +0530

    CRUD functionality update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,45 @@
-class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
+  class ArticlesController < ApplicationController
+    def show
+      @article = Article.find(params[:id])
+    end
+
+    def index
+      @articles = Article.all
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def create
+      #render plain: @article.inspect
+      @article = Article.new(params.required(:article).permit(:title, :description))
+      if @article.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article  #redirect_to article_path(@article)
+      else
+        render 'new'
+      end
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(params.required(:article).permit(:title, :description))
+        flash[:notice] = "Article was updated successfuly."
+        redirect_to @article
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      redirect_to articles_path
+    end
+   
   end
-  def index
-    @articles = Article.all
-  end
-end
