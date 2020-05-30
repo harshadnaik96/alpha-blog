cad0087c6fa08f9d9dec5dd670ff958f21fe13d1
Harshad Naik <naikharshad01@gmail.com>
2020-05-30 15:45:29 +0530

    Added one to many assosciation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@
     def create
       #render plain: @article.inspect
       @article = Article.new(article_params)
+      @article.user = User.first
       if @article.save
         flash[:notice] = "Article was created successfully."
         redirect_to @article  #redirect_to article_path(@article)
